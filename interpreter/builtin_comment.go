ed84b218b7f937bf14778e08b5e68ee5965ffdd4
Steve Kemp <steve@steve.org.uk>
2022-06-21 16:53:41 +0300

    Bugfix: we can't restrict the arguments to the comment-function, or comments break.

--- interpreter/builtin_comment.go
+++ interpreter/builtin_comment.go
@@ -1,13 +1,7 @@
 package interpreter
 
-import "fmt"
-
 // comment is a function which ignores comments "// xx" or "# xxxx".
 func comment(i *Interpreter, args []string) (string, error) {
-
-	if len(args) != 0 {
-		return "", fmt.Errorf("comment takes zero arguments")
-	}
 
 	return "", nil
 }
