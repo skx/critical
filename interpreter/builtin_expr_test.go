59ca1ac82cb93ae9b8fabebf5819879a145231f7
Steve Kemp <steve@steve.org.uk>
2022-06-22 08:24:17 +0300

    Added test-cases for eval + expr

---
+++ interpreter/builtin_expr_test.go
@@ -0,0 +1,67 @@
+package interpreter
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestExpr(t *testing.T) {
+
+	type TestCase struct {
+		Input  []string
+		Output string
+		Error  string
+	}
+
+	tests := []TestCase{
+		// basic maths
+		{Input: []string{"3", "+", "3"}, Output: "6"},
+		{Input: []string{"3", "*", "3"}, Output: "9"},
+		{Input: []string{"3", "/", "3"}, Output: "1"},
+		{Input: []string{"3", "-", "2"}, Output: "1"},
+
+		// >
+		{Input: []string{"3", ">", "2"}, Output: "1"},
+		{Input: []string{"1", ">", "2"}, Output: "0"},
+
+		// >=
+		{Input: []string{"3", ">=", "3"}, Output: "1"},
+		{Input: []string{"1", ">=", "2"}, Output: "0"},
+
+		// <
+		{Input: []string{"3", "<", "3"}, Output: "0"},
+		{Input: []string{"1", "<", "2"}, Output: "1"},
+
+		// <=
+		{Input: []string{"3", "<=", "3"}, Output: "1"},
+		{Input: []string{"21", "<=", "2"}, Output: "0"},
+
+		// ==
+		{Input: []string{"3", "==", "3"}, Output: "1"},
+		{Input: []string{"21", "==", "2"}, Output: "0"},
+
+		// errors
+		{Input: []string{"steve", "+", "3"}, Output: "", Error: "strconv"},
+		{Input: []string{"34", "+", "steve"}, Output: "", Error: "strconv"},
+		{Input: []string{"33", "%", "11"}, Output: "", Error: "unknown operation"},
+	}
+
+	for _, test := range tests {
+
+		e := New("")
+
+		out, err := expr(e, test.Input)
+		if err != nil {
+			if test.Error == "" {
+				t.Fatalf("error calling Eval(%s):%s", test.Input, err)
+			} else {
+				if !strings.Contains(err.Error(), test.Error) {
+					t.Fatalf("expected error, got the wrong one %s", err)
+				}
+			}
+		}
+		if out != test.Output {
+			t.Fatalf("unexpected output for Eval(%s) - got %s, but expected %s", test.Input, out, test.Output)
+		}
+	}
+}
--- interpreter/interpreter_test.go
+++ interpreter/interpreter_test.go
@@ -2,50 +2,6 @@ package interpreter
 
 import "testing"
 
-func TestEval(t *testing.T) {
-
-	type TestCase struct {
-		Input  string
-		Output string
-	}
-
-	tests := []TestCase{
-		{Input: `"Steve"`, Output: `Steve`},
-		{Input: `"33"`, Output: `33`},
-		{Input: `expr 3 + 3`, Output: `6`},
-		{Input: `expr 3 * 3`, Output: `9`},
-		{Input: `expr 3 - 1`, Output: `2`},
-		{Input: `expr 3 / 3`, Output: `1`},
-		{Input: `expr 3 <= 3`, Output: `1`},
-		{Input: `expr 4 <= 3`, Output: `0`},
-
-		{Input: `expr 3 < 3`, Output: `0`},
-		{Input: `expr 1 < 3`, Output: `1`},
-
-		{Input: `expr 3 > 3`, Output: `0`},
-		{Input: `expr 5 > 3`, Output: `1`},
-
-		{Input: `expr 3 >= 3`, Output: `1`},
-		{Input: `expr 1 >= 3`, Output: `0`},
-
-		{Input: `expr 1 == 3`, Output: `0`},
-		{Input: `expr 2 == 2`, Output: `1`},
-	}
-
-	for _, test := range tests {
-
-		e := New("")
-
-		out, err := e.Eval(test.Input)
-		if err != nil {
-			t.Fatalf("error calling Eval(%s):%s", test.Input, err)
-		}
-		if out != test.Output {
-			t.Fatalf("unexpected output for Eval(%s) - got %s, but expected %s", test.Input, out, test.Output)
-		}
-	}
-}
-
 func TestExpandEval(t *testing.T) {
 
 	x := New(`puts [ expr 1 + [ expr 2 + 3 ] ]`)
